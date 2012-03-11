bf05bf8a6cfe5659ad67958f8ad1f2b63c971325
eaturspinach <markmuskardin@gmail.com>
2012-03-10 16:55:25 -0800

    tweaks to modal form

--- app/assets/javascripts/backbone/views/login_view.js
+++ app/assets/javascripts/backbone/views/login_view.js
@@ -2,19 +2,22 @@ CoffeeChat.Views.LoginView = Backbone.View.extend({
   formElem: '#login_form',
   template: _.template(
     '<div id="login_modal">' +
-      '<form id="login_form">' +
-        '<div class="form_field">' +
-          '<label>Email:</label>' +
-          '<input id="email" name="email" type="email" />' +
-        '</div>' +
-        '<div class="form_field">' +
-          '<label>Name:</label>' +
-          '<input id="name" name="name" />' + 
-        '</div>' +
-        '<div class="form_field">' +
-          '<input id="login_submit" type="submit" value="Announce" />' +
-        '</div>' +
-      '</form>' +
+      '<div id="login_modal_container">' +
+        '<div id="message">Announce Yourself!</div>' +
+        '<form id="login_form">' +
+          '<div class="form_field">' +
+            '<label>Email:</label>' +
+            '<input id="email" name="email" type="email" />' +
+          '</div>' +
+          '<div class="form_field">' +
+            '<label>Name:</label>' +
+            '<input id="name" name="name" />' + 
+          '</div>' +
+          '<div class="form_field">' +
+            '<input id="login_submit" type="submit" value="Announce" />' +
+          '</div>' +
+        '</form>' +
+      '</div>' +
     '<div>'
   ),    
   events:{
@@ -28,7 +31,6 @@ CoffeeChat.Views.LoginView = Backbone.View.extend({
     return false;
   },
   render: function(){
-    console.log('inside login view');
     $(this.el).html(this.template());
     return this;
   }
