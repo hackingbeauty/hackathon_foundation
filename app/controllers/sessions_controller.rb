5e3c4621241cb519d157fa9c304717f685175b5a
eaturspinach <markmuskardin@gmail.com>
2012-05-16 20:46:44 -0700

    modified sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,35 +6,7 @@ class SessionsController < ApplicationController
     @title = "Sign In"
   end
 
-  def create
-    puts "============================"
-    puts "params[:user] #{params[:user]}"
-    latitude = params[:user][:coordinates]['latitude'].to_f
-    longitude = params[:user][:coordinates]['longitude'].to_f
 
-    user = User.find_or_create_by_email(params[:user])
-    puts "--------"
-    puts "the user is #{user}"
-    coordinate = Coordinate.find_or_create_by_latitude_and_longitude(latitude,longitude);
-    
-    puts "testing geocoder here"
-    puts "bearing to paris is #{coordinate.bearing_to("Paris, France")}"
-    
-    user.visits.create!(:coordinate_id => coordinate.id)
-
-    @users = User.all
-    puts "about to render #{@users}"
-
-    if user
-      sign_in user
-    end
-    
-    # respond_to do |format|   
-    #   format.json { render :json => { :users => @users } }  
-    # end    
-   render :json => @users
-   
-  end
 
   def destroy
   end
